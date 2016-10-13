c3bff9b2fa7ee54894c0f466567aacc38ffa270d
Rafał Burczyński <merfik18@gmail.com>
2016-10-13 13:44:25 +0200

    minor changes

--- Makefile
+++ Makefile
@@ -19,3 +19,6 @@ siparse.a:
 
 clean:
 	rm -f mshell *.o
+
+debug: CFLAGS+= -g
+debug: mshell
--- mshell.c
+++ mshell.c
@@ -19,8 +19,11 @@ static char *buf_end = buffer, *buf2_end = snd_buffer;
 static char *linepos = NULL;
 static struct stat fd_status;
 
-char* next_line();
-int read_line();
+static char* next_line();
+static inline char* find_end(char *start, char *end);
+static inline void copy_to_snd_buf(char *from, char *to);
+static int read_line();
+static int read_line_if_neccesary();
 
 int
 main(int argc, char *argv[])
@@ -41,12 +44,13 @@ main(int argc, char *argv[])
 
         // Read line
         nline = next_line();
-        if(*nline == '\0')
-            break;
         if(nline == NULL) {
             WRITES(STDERR_FILENO, SYNTAX_ERROR_STR);
             WRITES(STDERR_FILENO, "\n");
+            continue;
         }	
+        if(*nline == '\0')
+            break;
         puts(nline);
         // Parse
         line* l = parseline(nline);
@@ -85,66 +89,79 @@ main(int argc, char *argv[])
     return 0;
 }
 
+int read_line_if_neccesary(){
+    if(linepos == NULL || linepos == buf_end)
+        return read_line();
+    return buf_end - buffer;
+}
+
 int read_line(){
-    int status = read(STDIN_FILENO, buffer, MAX_LINE_LENGTH+1);
+    int status = read(STDIN_FILENO, buffer, MAX_LINE_LENGTH);
     if(status < 0)
         return status;
+    linepos = buffer;
     buf_end = buffer + status;
-    *buf_end = '\0';
+    // *buf_end = '\0';
     return status;
 }
 
-char* next_line(){
-    int l;
-    if(linepos == NULL) {
-        if((l = read_line()) < 0)
-            return NULL;
-        if(l == 0){
-            *buffer = '\0';
-            return buffer;
-        }
-        linepos = buffer;
+inline char* find_end(char *start, char *end){
+    while(*start != '\n' && start != end)
+        ++start;
+    return start;
+}
+
+inline void copy_to_snd_buf(char *from, char *to){
+    if((to-from) + (buf2_end - snd_buffer) > MAX_LINE_LENGTH)
+        buf2_end = snd_buffer;
+    else {
+        memcpy(buf2_end, from, sizeof(char)*(to-from));
+        buf2_end += (to-from);
     }
-    char *ret = linepos;
-    while(ret < buf_end && *ret != '\n')
-        ++ret;
+}
+
+char* next_line(){
+
+    int l = read_line_if_neccesary();
+    if(l < 0)
+        return NULL;
+    if(l == 0){
+        *buffer = '\0';
+        return buffer;
+    }
+
+    char *ret = find_end(linepos, buf_end);
 
     // command ended
 
-    if(*ret == '\n'){
+    if(ret != buf_end){
         *ret = '\0';
         char *x = linepos;
-        linepos = ret+1 >= buf_end ? NULL : ret+1;
+        linepos = ret+1;
         return x;
     }
 
     // command didn't end
 
     buf2_end = snd_buffer;
-    memcpy(snd_buffer, linepos, sizeof(char)*(ret-linepos));
-    buf2_end += ret-linepos;
+    copy_to_snd_buf(linepos, ret);
     while(1) {
-        if(buf2_end - snd_buffer > MAX_LINE_LENGTH)
-            return NULL;
         if((l = read_line()) < 0)
             return NULL;
-        if(l == 0){
+        /*if(l == 0){
             linepos = NULL;
             *buf2_end = '\0';
             return snd_buffer;
-        }
-        buf_end = buffer + l;
-        linepos = buffer;
-        for(ret = linepos; ret < buf_end && *ret != '\n'; ++ret);
-        if(ret - linepos + (buf2_end - snd_buffer) > MAX_LINE_LENGTH){
-            linepos = ret+1 >= buf_end ? NULL : ret+1;
+        }*/
+        ret = find_end(linepos, buf_end);
+        copy_to_snd_buf(linepos, ret);
+        if(buf2_end == snd_buffer){
+            linepos = (ret == buf_end ? NULL : ret+1);
             return NULL;
         }
-        memcpy(buf2_end, linepos, sizeof(char)*(ret-linepos));
-        buf2_end += ret-linepos;
-        if(*ret == '\n'){
+        if(ret != buf_end){
             *buf2_end = '\0';
-            linepos = ret+1 >= buf_end ? NULL : ret+1;
+            linepos = ret+1;
             return snd_buffer;
         }
     }
